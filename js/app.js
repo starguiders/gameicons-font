5c1af207bf39077e24630d4952d08d35244bbc8e
starguiders <starguiders@users.noreply.github.com>
2026-09-06 11:55:19 +0000

    Deploy to GitHub pages

---
+++ js/app.js
@@ -0,0 +1,98 @@
+const Data = {
+    glyphs: () => m.request({ method: 'GET', url: 'data/glyphs.json' })
+};
+
+const Filter = {
+    controller: function() {
+        this.searchTerm = m.prop('');
+    },
+    view: (ctrl) => {
+        return m('input.form-control', { placeholder: 'Filtrar iconos...', oninput: m.withAttr('value', ctrl.searchTerm) });
+    }
+};
+
+const ForegroundColor = {
+    controller: function() {
+        this.fgColor = m.prop('');
+    },
+    view: (ctrl) => {
+        return m('input.form-control', { placeholder: 'Color de los iconos', oninput: m.withAttr('value', ctrl.fgColor) });
+    }
+};
+
+const List = {
+    controller: function(options) {
+        return {
+            glyphs: Data.glyphs(),
+            visible: options.visible,
+            fgColor: options.fgColor
+        };
+    },
+    view: (ctrl) => {
+        return ctrl
+            .glyphs()
+            .sort((a, b) => a.file > b.file ? 1 : a.file < b.file ? -1 : 0)
+            .filter(ctrl.visible)
+            .map(glyph => m('div.col-sm-2.icon-block', [
+                m(`i.` + fontClass + `.` + iconClass + `-${glyph.file}.icon-md`, { style: { color: ctrl.fgColor() }}),
+                m('br'),
+                m(`a[target="_blank"][href="./svg/${glyph.file}.svg"]`, glyph.file),
+                m('br'),
+                m(`span.icon-code.` + iconClass +`-${glyph.file}`, {
+                         // jQuery function to generate unicode
+                         config : unicodeShow
+                       }
+                 )
+            ]));
+    }
+};
+
+function unicodeShow (element, init, context){
+  // We don't want to add the class all the time, only the first time the element is created
+  if(!init){
+    // Here we reference the element directly, and pass it to jQuery
+	$(element).each(function(){
+		var s = window.getComputedStyle(element, ':before').getPropertyValue('content'),
+		char = '\\' + s.charCodeAt(1).toString(16);
+		$(element).html(char);
+	});	
+  }
+}
+
+const Page = {
+    controller: function() {
+        this.list = List.controller({
+            visible: (item) => item.file.indexOf(this.filter.searchTerm()) > -1,
+            fgColor: () => this.foregroundColor.fgColor()
+        });
+
+        this.foregroundColor = new ForegroundColor.controller();
+        this.filter = new Filter.controller();
+    },
+    view: (ctrl) => {
+        return [
+            m('div.row.text-center', [
+                m('div.col-sm-6', Filter.view(ctrl.filter)),
+                m('div.col-sm-6', ForegroundColor.view(ctrl.foregroundColor))
+            ]),
+            m('div.row', List.view(ctrl.list))
+        ];
+    }
+};
+
+m.mount(document.getElementById('page'), Page);
+
+/*
+// Old jQuery function to generate unicode outside of the Mithril code 
+document.addEventListener('DOMContentLoaded', () => {
+  	setInterval(function(){
+		$('.icon-block .icon-code').each(function(){
+			var s = window.getComputedStyle(this, ':before').getPropertyValue('content'),
+			char = '\\' + s.charCodeAt(1).toString(16);
+			$(this).html(char);
+		});	
+	},10);  
+});
+*/
+const fontClass = "game-icon";
+const iconClass = "game-icon";
